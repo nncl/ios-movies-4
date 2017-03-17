c798b84a3c99c5a05ecae1ebfdd95187db21f581
Usuário Convidado <Guest@PA06MICRO16.local>
2017-03-17 19:58:47 -0300

    Updating view flow

---
+++ MoviesLib/MovieRegisterViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class MovieRegisterViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var tfTitle: UITextField!
+    @IBOutlet weak var tfScore: UITextField!
+    @IBOutlet weak var lbCategories: UILabel!
+    @IBOutlet weak var tfDuration: UITextField!
+    @IBOutlet weak var tvSummary: UITextView!
+    @IBOutlet weak var ivPoster: UIImageView!
+    @IBOutlet weak var btAddUpdate: UIButton!
+    
+    // MARK: - Functions
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        }
+
+    @IBAction func close(_ sender: UIButton) {
+    }
+    @IBAction func addUpdateMovie(_ sender: UIButton) {
+    }
+    
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 # Movies 4
+
+- Core Data, which uses SQLite under the shit
